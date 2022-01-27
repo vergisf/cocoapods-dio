214f292cda2e54618569987c0f52296966ced592
Fernando Vergis <vergisf@gmail.com>
2022-01-27 17:23:03 -0300

    Projeto base
    Inserindo projeto base

---
+++ Pods/Target Support Files/Pods-cocoapods-dio-cocoapods-dioUITests/Pods-cocoapods-dio-cocoapods-dioUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cocoapods_dio_cocoapods_dioUITests : NSObject
+@end
+@implementation PodsDummy_Pods_cocoapods_dio_cocoapods_dioUITests
+@end
---
+++ Pods/Target Support Files/Pods-cocoapods-dioTests/Pods-cocoapods-dioTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cocoapods_dioTests : NSObject
+@end
+@implementation PodsDummy_Pods_cocoapods_dioTests
+@end
